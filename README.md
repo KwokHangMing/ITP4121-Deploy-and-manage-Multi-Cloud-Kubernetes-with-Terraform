70348a8e6a465477109ea87ecff6c4e9530102b8
Leo Kwok <112677134+KwokHangMing@users.noreply.github.com>
2024-03-29 07:43:11 +0000

    Add devcontainer customizations and deployment scripts

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ITP4121-Deploy-and-manage-Multi-Cloud-Kubernetes-with-Terraform
+
+
+gcloud auth application-default login
+gcloud auth application-default set-quota-project <PROJECT_ID>
+
+az login --use-device-code
+az account show
---
+++ deploy.sh
@@ -0,0 +1,26 @@
+echo "Please enter the cloud provider (aws, azure, gcp) or 'exit' to stop: "
+read provider
+
+case $provider in
+  aws)
+    cd aws
+    ;;
+  azure)
+    cd azure
+    ;;
+  gcp)
+    cd gcp
+    ;;
+  exit)
+    echo "Exiting script."
+    exit 0
+    ;;
+  *)
+    echo "Invalid option. Exiting script."
+    exit 1
+    ;;
+esac
+
+terraform init -upgrade
+# terraform plan
+terraform apply --auto-approve
